15fc0cb50ca7f2a7f32598d385f510885650fc0b
JemjemfromYT <saavedrajames10@gmail.com>
2025-05-14 18:25:03 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+let hotdogId = document.getElementById ("hotdogId")
+let ahh = document.getElementById ("ahh")
+let chenyaoVoice = document.getElementById ("chenyaoVoice")
+let createdBy = document.getElementById ("createdBy")
+let go = document.getElementById ("go")
+let show = document.getElementById ("show")
+
+
+go.addEventListener('click', function(){
+show.style.display="block"
+go.style.display="none"
+
+setTimeout(()=>{
+  hotdogId.classList.add("follow")
+  console.log("follow")
+  createdBy.style.opacity="0";
+},6000);
+
+setTimeout(()=>{
+  boom.play()
+},0001);
+
+setTimeout(()=>{
+ reload.play()
+},5000);
+
+setTimeout(()=>{
+chenyao.style.opacity="1"
+chenyaoVoice.play()
+setTimeout(()=>{
+milk.style.opacity="1"
+squirt.play()
+setTimeout(()=>{
+chenyao2.style.opacity="1"
+chenyao.style.opacity="0"
+milk.style.opacity="0"
+ahh.play()
+setTimeout(()=>{
+  document.body.style.opacity="0"
+},1000);
+},1300);
+},1300);
+},8000);
+
+});
